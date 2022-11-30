305fae6cdd1e9c8ac1740059b1eec16eb228a0db
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-30 15:00:25 +0500

    update logic

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-
 import CartItem from './CartItem/CartItem';
 import useStyles from './styles';
 
 const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
   const classes = useStyles();
-
   const handleEmptyCart = () => onEmptyCart();
-
   const renderEmptyCart = () => (
     <Typography variant="subtitle1">You have no items in your shopping cart,
       <Link className={classes.link} to="/">start adding some</Link>!
