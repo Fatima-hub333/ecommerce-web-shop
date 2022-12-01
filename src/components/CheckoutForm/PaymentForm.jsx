d94c2a2e174a2ecc725690e76ec6f7ecdd35d42c
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-12-01 14:09:11 +0500

    done with checkout first part

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CardElements, ElementsConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import Review from './Review';
 
 const PaymentForm = () => {
   return (
-    <div>PaymentForm</div>
+    <>
+      <Review />
+    </>
   )
 }
 
